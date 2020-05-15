1274a2899ba9b0c46911655694105748300fac08
Kevin Silvestre <kevsilvo@gmail.com>
2020-05-15 11:18:57 -0400

    added conditionals on story.pgp

--- story.php
+++ story.php
@@ -1,10 +1,53 @@
 <?php
 session_start();
-$word1 = htmlspecialchars($_SESSION['word'][1]);
-$word2 = htmlspecialchars($_SESSION['word'][2]);
-$word3 = htmlspecialchars($_SESSION['word'][3]);
-$word4 = htmlspecialchars($_SESSION['word'][4]);
-$word5 = htmlspecialchars($_SESSION['word'][5]);
+
+if(isset($_SESSION['word'][1])){
+    $word1 = htmlspecialchars($_SESSION['word'][1]);
+    trim($word1);
+} else if(empty($_SESSION['word'][1])){
+    header('location: play.php');
+} else {
+    header('location: play.php');
+}
+
+if(isset($_SESSION['word'][2])){
+    $word2 = htmlspecialchars($_SESSION['word'][2]);
+    trim($word2);
+} else if(empty($_SESSION['word'][2])){
+    header('location: play.php?p=2');
+} else {
+    header('location: play.php?p=2');
+}
+
+if(isset($_SESSION['word'][3])){
+    $word3 = htmlspecialchars($_SESSION['word'][3]);
+    trim($word3);
+} else if(empty($_SESSION['word'][3])){
+    header('location: play.php?p=3');
+} else {
+    header('location: play.php?p=3');
+}
+
+if(isset($_SESSION['word'][4])){
+    $word4 = htmlspecialchars($_SESSION['word'][4]);
+    trim($word4);
+} else if(empty($_SESSION['word'][4])){
+    header('location: play.php?p=4');
+} else {
+    header('location: play.php?p=4');
+}
+
+if(isset($_SESSION['word'][5])){
+    $word5 = htmlspecialchars($_SESSION['word'][5]);
+    trim($word5);
+} else if(empty($_SESSION['word'][5])){
+    header('location: play.php?p=5');
+} else {
+    header('location: play.php?p=5');
+}
+
+
+
 
 include 'inc/header.php';
 
